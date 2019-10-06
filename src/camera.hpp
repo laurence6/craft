4bb5698e15e9d5c6ddc06c744d04b59c3a3d2407
Laurence <liuxy6@gmail.com>
2019-10-06 01:57:17 -0400

    Update Camera

--- src/camera.hpp
+++ src/camera.hpp
@@ -16,11 +16,11 @@ private:
     float rot = 0.f; // rotation around z axis / yaw
     float pitch = 90.f;
 
-    vec3 cam_d    = vec3(1.f, 0.f, 0.f);
-    vec3 player_f = vec3(1.f, 0.f, 0.f);
-    vec3 player_l = vec3(0.f, 1.f, 0.f);
+    vec3 forward = vec3(1.f, 0.f, 0.f);
+    vec3 player_forward = vec3(1.f, 0.f, 0.f);
+    vec3 player_left = vec3(0.f, 1.f, 0.f);
     float v_forward = 0.f;
-    float v_left    = 0.f;
+    float v_left = 0.f;
 
 public:
     void init() {
@@ -29,45 +29,49 @@ public:
         collider = new Collider(0.003f, 0.000f, 0.0195f);
     }
 
-#define MOVE(d, v, as1, as2)                                                                           \
+    vec3 const& get_forward() const {
+        return forward;
+    }
+
+#define MOVE_FUNC(d, v, as1, as2)                                                                      \
     void start_move_ ## d () { v = clamp(v as1 cam_speed, -cam_speed, cam_speed); update_velocity(); } \
     void stop_move_  ## d () { v = clamp(v as2 cam_speed, -cam_speed, cam_speed); update_velocity(); }
 
-    MOVE(forward,  v_forward, +, -)
+    MOVE_FUNC(forward,  v_forward, +, -)
 
-    MOVE(backward, v_forward, -, +)
+    MOVE_FUNC(backward, v_forward, -, +)
 
-    MOVE(left,  v_left, +, -)
+    MOVE_FUNC(left,  v_left, +, -)
 
-    MOVE(right, v_left, -, +)
+    MOVE_FUNC(right, v_left, -, +)
 
 #undef MOVE
 
     void rotate(float del_x, float del_y) {
         rot = fmod(rot + del_x * cam_rot_speed, 360.f);
         pitch = clamp(pitch + del_y * cam_rot_speed, 1.f, 179.f);
-        cam_d = normalize(vec3(
+        forward = normalize(vec3(
             sin(radians(pitch)) * cos(radians(rot)),
             sin(radians(pitch)) * sin(radians(rot)),
             cos(radians(pitch))
         ));
 
-        player_f = normalize(vec3(cos(radians(rot)), sin(radians(rot)), 0));
-        player_l = cross(vec3(0.f, 0.f, 1.f), player_f);
+        player_forward = normalize(vec3(cos(radians(rot)), sin(radians(rot)), 0));
+        player_left = cross(vec3(0.f, 0.f, 1.f), player_forward);
 
         update_velocity();
     }
 
     mat4 get_mvp() const {
         const mat4 projection = perspective(FOVY, ASPECT, Z_NEAR, Z_FAR);
-        mat4 view = lookAt(pos, pos + cam_d, vec3(0.f, 0.f, 1.f));
+        mat4 view = lookAt(pos, pos + forward, vec3(0.f, 0.f, 1.f));
         mat4 mvp = projection * view;
         return mvp;
     }
 
 private:
     void update_velocity() {
-        vec3 new_v = player_f * v_forward + player_l * v_left;
+        vec3 new_v = player_forward * v_forward + player_left * v_left;
         new_v.z = 0.f;
         float len = length(new_v);
         if (len > 0.f) {
